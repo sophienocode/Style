5140a66c9547a6a4c00a055a20b1fab107228dad
sophienocode <sophieprotanki@gmail.com>
2026-02-07 07:07:55 -0800

    (Auth): fix smth in code

--- src/components/StyleCard.jsx
+++ src/components/StyleCard.jsx
@@ -9,11 +9,13 @@ export default function StyleCard() {
   return (
     <>
       <section style={{ backgroundColor: '#89CFF0', padding: '20px',borderRadius: '10px', color: 'black' }} >
-      <FaCartArrowDown />
-      <FaComputer />  
-
+     
       <div>
-        <h1>Style Card</h1>
+        <h1>
+          Style Card
+        <FaCartArrowDown />
+        <FaComputer />  
+        </h1>
         Compenent with Inline Styles  
       </div>  
       </section>  
